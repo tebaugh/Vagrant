ef55b00a52a0b70d3ada00774086931c28350903
Terry Ebaugh <tebaugh@gmail.com>
2014-04-03 14:48:54 -0400

    Used generate_series instead on a temp table

--- seven_db/code/postgres/calendar.sql
+++ seven_db/code/postgres/calendar.sql
@@ -1,7 +1,7 @@
 -- Builds a calendar of how many events exist in a year
 SELECT * FROM crosstab(
   'SELECT extract(year from starts) as year, extract(month from starts) as month, count(*) FROM events GROUP BY year, month',
-	'SELECT * FROM month_count'
+	'SELECT * FROM generate_series(1,12)'
 ) AS (
   year int,
   jan int, feb int, mar int, apr int, may int, jun int, jul int, aug int, sep int, oct int, nov int, dec int
